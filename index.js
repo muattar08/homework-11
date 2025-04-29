f56f29431319c7a47f36195f67c9b53daf892231
muattar08 <muattarerkinova012@gmail.com>
2025-04-29 13:11:03 +0500

    Обновлён JS: добавлен эффект появления элементов при скролле

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Массив пользователей
 let users = [
     {
         "id": 1,
@@ -10,10 +9,7 @@ let users = [
             "suite": "Apt. 556",
             "city": "Gwenborough",
             "zipcode": "92998-3874",
-            "geo": {
-                "lat": "-37.3159",
-                "lng": "81.1496"
-            }
+            "geo": { "lat": "-37.3159", "lng": "81.1496" }
         },
         "phone": "1-770-736-8031 x56442",
         "website": "hildegard.org",
@@ -33,10 +29,7 @@ let users = [
             "suite": "Suite 879",
             "city": "Wisokyburgh",
             "zipcode": "90566-7771",
-            "geo": {
-                "lat": "-43.9509",
-                "lng": "-34.4618"
-            }
+            "geo": { "lat": "-43.9509", "lng": "-34.4618" }
         },
         "phone": "010-692-6593 x09125",
         "website": "anastasia.net",
@@ -56,10 +49,7 @@ let users = [
             "suite": "Suite 847",
             "city": "McKenziehaven",
             "zipcode": "59590-4157",
-            "geo": {
-                "lat": "-68.6102",
-                "lng": "-47.0653"
-            }
+            "geo": { "lat": "-68.6102", "lng": "-47.0653" }
         },
         "phone": "1-463-123-4447",
         "website": "ramiro.info",
@@ -79,10 +69,7 @@ let users = [
             "suite": "Apt. 692",
             "city": "South Elvis",
             "zipcode": "53919-4257",
-            "geo": {
-                "lat": "29.4572",
-                "lng": "-164.2990"
-            }
+            "geo": { "lat": "29.4572", "lng": "-164.2990" }
         },
         "phone": "493-170-9623 x156",
         "website": "kale.biz",
@@ -102,10 +89,7 @@ let users = [
             "suite": "Suite 351",
             "city": "Roscoeview",
             "zipcode": "33263",
-            "geo": {
-                "lat": "-31.8129",
-                "lng": "62.5342"
-            }
+            "geo": { "lat": "-31.8129", "lng": "62.5342" }
         },
         "phone": "(254)954-1289",
         "website": "demarco.info",
@@ -125,10 +109,7 @@ let users = [
             "suite": "Apt. 950",
             "city": "South Christy",
             "zipcode": "23505-1337",
-            "geo": {
-                "lat": "-71.4197",
-                "lng": "71.7478"
-            }
+            "geo": { "lat": "-71.4197", "lng": "71.7478" }
         },
         "phone": "1-477-935-8478 x6430",
         "website": "ola.org",
@@ -148,10 +129,7 @@ let users = [
             "suite": "Suite 280",
             "city": "Howemouth",
             "zipcode": "58804-1099",
-            "geo": {
-                "lat": "24.8918",
-                "lng": "21.8984"
-            }
+            "geo": { "lat": "24.8918", "lng": "21.8984" }
         },
         "phone": "210.067.6132",
         "website": "elvis.io",
@@ -171,10 +149,7 @@ let users = [
             "suite": "Suite 729",
             "city": "Aliyaview",
             "zipcode": "45169",
-            "geo": {
-                "lat": "-14.3990",
-                "lng": "-120.7677"
-            }
+            "geo": { "lat": "-14.3990", "lng": "-120.7677" }
         },
         "phone": "586.493.6943 x140",
         "website": "jacynthe.com",
@@ -194,10 +169,7 @@ let users = [
             "suite": "Suite 449",
             "city": "Bartholomebury",
             "zipcode": "76495-3109",
-            "geo": {
-                "lat": "24.6463",
-                "lng": "-168.8889"
-            }
+            "geo": { "lat": "24.6463", "lng": "-168.8889" }
         },
         "phone": "(775)976-6794 x41206",
         "website": "conrad.com",
@@ -217,10 +189,7 @@ let users = [
             "suite": "Suite 198",
             "city": "Lebsackbury",
             "zipcode": "31428-2261",
-            "geo": {
-                "lat": "-38.2386",
-                "lng": "57.2232"
-            }
+            "geo": { "lat": "-38.2386", "lng": "57.2232" }
         },
         "phone": "024-648-3804",
         "website": "ambrose.net",
@@ -237,20 +206,20 @@ let emails = {
     net: [],
     info: []
 };
-
 let other = [];
 
 for (let user of users) {
-    let domain = user.email.split('@')[1].split('.')[0];  
+    let domain = user.email.split('@')[1]; 
+    let suffix = domain.split('.').pop();  
 
-    if (emails[domain]) {
-        emails[domain].push(user);
+    if (emails.hasOwnProperty(suffix)) {
+        emails[suffix].push(user);
     } else {
         other.push(user);
     }
 }
 
-console.log("Организация: ", emails.org);
-console.log("Инфо: ", emails.info);
-console.log("Нет: ", emails.net);
-console.log("Прочие: ", other);
+console.log("ORG:", emails.org);
+console.log("NET:", emails.net);
+console.log("INFO:", emails.info);
+console.log("ДРУГОЕ:", other);
